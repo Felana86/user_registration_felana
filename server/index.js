c70cde25de3798419154cb45bf05c5a56813eff1
felana86 <felana0695608707@gmail.com>
2022-02-24 22:42:42 +0100

    adding login router to my server app

--- server/index.js
+++ server/index.js
@@ -36,6 +36,26 @@ app.post('/register', (req, res) => {
     );
 })
 
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.execute(
+        "SELECT * FROM users WHERE username = ? AND password = ?",
+        [username, password],
+        (err, result) => {
+            if (err) {
+                res.send({err: err});
+            }
+            if (result.length > 0) {
+                res.send(result);
+            } else {
+                ({message: "Wrong username/password combination:"});
+            }
+        }
+    );
+});
+
 app.use(express.json());
 
 app.listen(5050, () => {
